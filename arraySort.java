7caf256fb376ea86ea81f45652d7a420da6c0b5b
George <gchriscarter@gmail.com>
2021-11-16 15:56:37 -0500

    add arraySort.java sorting usuing the swap method with working solution

---
+++ arraySort.java
@@ -0,0 +1,17 @@
+public class arraySort {
+  public static void main(String[] args) {
+    int arr[] = { 2, 4, 3, 1 };
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i + 1; j < arr.length; j++) {
+        int temp = 0;
+        if (arr[j] < arr[i]) {
+          temp = arr[i];
+          arr[i] = arr[j];
+          arr[j] = temp;
+        }
+      }
+      System.out.print(arr[i] + " ");
+    }
+  }
+
+}
