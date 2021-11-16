406ea1054dfe319b28decc60e4aa8f5a8a4aee5f
George <gchriscarter@gmail.com>
2021-11-16 13:59:57 -0500

    add arrayReverse.java using the swap method with working solution

---
+++ arrayReverse.java
@@ -0,0 +1,23 @@
+// Reversing an Array using the swap method, i.e. Swapping first element with last, second element with second to last element, etc. Less moves than new array method
+
+public class arrayReverse {
+
+  static void reverse(int a[], int n) {
+    int i, k, t;
+    for (i = 0; i < n / 2; i++) {
+      t = a[i];
+      a[i] = a[n - i - 1];
+      a[n - i - 1] = t;
+    }
+    System.out.println("Reversed array is: \n");
+    for (k = 0; k < n; k++) {
+      System.out.println(a[k]);
+    }
+  }
+
+  public static void main(String[] args) {
+    int[] arr = { 1, 2, 3, 4, 5 };
+    reverse(arr, arr.length);
+  }
+
+}
